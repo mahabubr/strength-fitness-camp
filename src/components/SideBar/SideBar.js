e45c3a454fc9b1c875a596522702f44b7a4374e0
mahabubr <mahabub.r753@gmail.com>
2022-09-29 09:59:07 +0600

    profile section complete

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './SideBar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
+import SidebarProfile from './SidebarProfile/SidebarProfile';
 
 const SideBar = () => {
     return (
         <div className='sidebar'>
-            <div className='sidebar-profile'>
-                <img src="https://www.upwork.com/profile-portraits/c14G8XFK9OOmlN0EWO14fxi3Y8-vy73LCdxNSEcCG1TWZaCt1UePSCuTXOO4-SDxp-" alt="" />
-                <div className='profile-info'>
-                    <h3 className='profile-name'>Mahabubur Rahman</h3>
-                    <p className='profile-location'>
-                        <FontAwesomeIcon icon={faLocationArrow} />
-                        <span className='location-info'>Bogra, Bangladesh</span>
-                    </p>
-                </div>
-            </div>
+            <SidebarProfile />
         </div>
     );
 };
---
+++ src/components/SideBar/SidebarProfile/SidebarProfile.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './SidebarProfile.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
+
+const SidebarProfile = () => {
+    return (
+        <div>
+            <div className='sidebar-profile'>
+                <img src="https://www.upwork.com/profile-portraits/c14G8XFK9OOmlN0EWO14fxi3Y8-vy73LCdxNSEcCG1TWZaCt1UePSCuTXOO4-SDxp-" alt="" />
+                <div className='profile-info'>
+                    <h3 className='profile-name'>Mahabubur Rahman</h3>
+                    <p className='profile-location'>
+                        <FontAwesomeIcon icon={faLocationArrow} />
+                        <span className='location-info'>Bogra, Bangladesh</span>
+                    </p>
+                </div>
+            </div>
+            <div className="profiles-info">
+                <div className="profile-content">
+                    <h2>62 kg</h2>
+                    <p>Weight</p>
+                </div>
+                <div className="profile-content">
+                    <h2>5.9</h2>
+                    <p>Height</p>
+                </div>
+                <div className="profile-content">
+                    <h2>25yrs</h2>
+                    <p>Age</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarProfile;
